afba98f232f71f6747889a8b4dcefc37867ad0e3
frederikjo <fredejohs@gmail.com>
2020-05-14 14:39:43 +0200

    fix: styled components

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const H1 = styled.h1`
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
 const Chart = () => {
-    return <h1>Chart</h1>
+    return <H1>Chart</H1>
 }
 
 export default Chart;
